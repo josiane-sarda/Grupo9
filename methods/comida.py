70999d48f146d5e6691ed986efbf9177e57d3dc2
Gkramer1 <55982430+Gkramer1@users.noreply.github.com>
2019-10-15 20:44:53 -0300

    tiltei

--- methods/comida.py
+++ methods/comida.py
@@ -1,15 +1,38 @@
 class Comida:
     def __init__(self):
-        self.__id= 0
-        self.__pizza=''
-        self.__hamburger=''
-        self.__suchi=''
-        self.__picanha_defumada_com_risoto=''
-        self.__bixcoito_carioca=''
-        self.__batata_frita=''
-        self.__pao_de_queijo=''
-        self.__frango_frito=''
-        self.__lasanha=''
-        self.__fricasse=''
-        self.__tapioca=''
-        
+        self.__id = 0
+        self.__pizza = ''
+        self.__hamburger = ''
+        self.__sushi = ''
+        self.__picanha_defumada_com_risoto = ''
+        self.__bixcoito_carioca = ''
+        self.__batata_frita = ''
+        self.__pao_de_queijo = ''
+        self.__frango_frito = ''
+        self.__lasanha = ''
+        self.__fricasse = ''
+        self.__tapioca = ''
+
+    @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self, id):
+        self.__id = id
+
+    @property
+    def pizza(self):
+        return self.__pizza
+
+    @tipo.setter
+    def pizza(self, pizza):
+        self.__tipo = __pizza
+
+    @property
+    def hamburger(self):
+        return self.__hamburger
+
+    @descricao.setter
+    def hamburger(self, hamburger):
+        self.__descricao = __hamburger
---
+++ methods/pagina.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+# from comida import Comida
+# from bebida import Bebida
+# from sobremesa import Sobremesa
+
+
+pagina_nome = "Nome da pagina"
+
+
+app = Flask(__name__)
+@app.route('/')
+def inicio():
+    return render_template('index.html')
+
+
+app.run()
