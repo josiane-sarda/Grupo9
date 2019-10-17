99c632488f67ad4b42569ede14703395473061a0
Josiane Sarda <josiane.sarda@gmail.com>
2019-10-16 23:42:32 -0300

    juncao py e html

--- methods/comida.py
+++ methods/comida.py
@@ -1,105 +1,143 @@
-# class Comida:
-#     def __init__(self):
-#         self.__id = 0
-#         self.__pizza = ''
-#         self.__hamburger = ''
-#         self.__sushi = ''
-#         self.__bixcoito = ''
-#         self.__batata = ''
-#         self.__pao = ''
-#         self.__frango = ''
-#         self.__lasanha = ''
-#         self.__fricasse = ''
-#         self.__tapioca = ''
+from flask import Flask, render_template, request, redirect
+import MySQLdb
 
-#     @property
-#     def id(self):
-#         return self.__id
+class Comida:
+    def __init__(self):
+        self.__id = 0
+        self.__pizza = ''
+        self.__hamburguer = ''
+        self.__sushi = ''
+        self.__bixcoito = ''
+        self.__batata = ''
+        self.__pao = ''
+        self.__frango = ''
+        self.__lasanha = ''
+        self.__fricasse = ''
+        self.__tapioca = ''
 
-#     @id.setter
-#     def id(self, id):
-#         self.__id = id
+    @property
+    def id(self):
+        return self.__id
 
-#     @property
-#     def pizza(self):
-#         return self.__pizza
+    @id.setter
+    def id(self, id):
+        self.__id = id
 
-#     @pizza.setter
-#     def pizza(self, pizza):
-#         self.__pizza = pizza
+    @property
+    def pizza(self):
+        return self.__pizza
 
-#     @property
-#     def hamburger(self):
-#         return self.__hamburger
+    @pizza.setter
+    def pizza(self, pizza):
+        self.__pizza = pizza
 
-#     @hamburger.setter
-#     def hamburger(self, hamburger):
-#         self.__hamburguer = hamburger
+    @property
+    def hamburguer(self):
+        return self.__hamburguer
 
-#     @property
-#     def sushi(self):
-#         return self.__sushi
+    @hamburguer.setter
+    def hamburguer(self, hamburguer):
+        self.__hamburguer = hamburguer
 
-#     @sushi.setter
-#     def sushi(self, sushi):
-#         self.__sushi = sushi
+    @property
+    def sushi(self):
+        return self.__sushi
 
-#     @property
-#     def bixcoito(self):
-#         return self.__bixcoito
+    @sushi.setter
+    def sushi(self, sushi):
+        self.__sushi = sushi
 
-#     @bixcoito.setter
-#     def bixcoito(self, bixcoito):
-#         self.__bixcoito = bixcoito
+    @property
+    def bixcoito(self):
+        return self.__bixcoito
 
-#     @property
-#     def batata(self):
-#         return self.__batata
+    @bixcoito.setter
+    def bixcoito(self, bixcoito):
+        self.__bixcoito = bixcoito
 
-#     @batata.setter
-#     def __batata(self, batata):
-#         self.__batata = batata
+    @property
+    def batata(self):
+        return self.__batata
 
-#     @property
-#     def pao(self):
-#         return self.__pao
+    @batata.setter
+    def __batata(self, batata):
+        self.__batata = batata
 
-#     @pao.setter
-#     def pao(self, pao):
-#         self.__pao = pao
+    @property
+    def pao(self):
+        return self.__pao
 
-#     @property
-#     def frango(self):
-#         return self.__frango
+    @pao.setter
+    def pao(self, pao):
+        self.__pao = pao
 
-#     @frango.setter
-#     def frango(self, frango):
-#         self.__frango = frango
+    @property
+    def frango(self):
+        return self.__frango
 
-#     @property
-#     def lasanha(self):
-#         return self.__lasanha
+    @frango.setter
+    def frango(self, frango):
+        self.__frango = frango
 
-#     @lasanha.setter
-#     def lasanha(self, lasanha):
-#         self.__lasanha = lasanha
+    @property
+    def lasanha(self):
+        return self.__lasanha
 
-#     @property
-#     def fricasse(self):
-#         return self.__fricasse
+    @lasanha.setter
+    def lasanha(self, lasanha):
+        self.__lasanha = lasanha
 
-#     @fricasse.setter
-#     def fricasse(self, fricasse):
-#         self.__fricasse = fricasse
+    @property
+    def fricasse(self):
+        return self.__fricasse
 
-#     @property
-#     def tapioca(self):
-#         return self.__tapioca
+    @fricasse.setter
+    def fricasse(self, fricasse):
+        self.__fricasse = fricasse
+       
+    @property
+    def tapioca(self):
+        return self.__tapioca
 
-#     @tapioca.setter
-#     def tapioca(self, tapioca):
-#         self.__tapioca = tapioca 
-    
+    @tapioca.setter
+    def tapioca(self, tapioca):
+        self.__tapioca = tapioca 
 
 
+def listar_comida_db():
+    conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae14", passwd="grupo09", database="zuplae14")
+    cursor = conexao.cursor()
+    cursor.execute("select * from COMIDA") 
+    listar_comida = []
+    for i in cursor.fetchall():
+        comida = Comida()
+        comida.id = i[0]
+        comida.pizza = i[1]
+        comida.hamburguer = i[2]
+        comida.suchi = i[3]
+        comida.bixcoito = i[4]
+        comida.batata = i[5]
+        comida.pao = i[6]
+        comida.frango = i[7]
+        comida.lasanha = i[8]
+        comida.fricasse = i[9]
+        comida.tapioca = i[10]
+        listar_comida.append(comida)
 
+    conexao.close()
+    return listar_comida
+
+def editar_comida_db(comida):
+    conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae04", passwd="lendas19", database="zuplae04")
+    cursor = conexao.cursor()
+    cursor.execute("UPDATE COMIDA SET NOME = '{}', QUANTIDADE = '{}' WHERE ID = {}"
+    .format(comida.nome, comida.quantidade, comida.id))
+    conexao.commit()
+    conexao.close()   
+
+def deletar_comida(id):
+    conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae04", passwd="lendas19", database="zuplae04")
+    cursor = conexao.cursor()
+    cursor.execute("DELETE FROM Comida WHERE id={}".format(id))
+    conexao.commit()
+    conexao.close()
--- pagina.py
+++ pagina.py
@@ -1,64 +1,39 @@
-from flask import Flask, render_template
-# from methods.comida import Comida
-# from methods.bebida import Bebida
-# from methods.sobremesa import Sobremesa
-# import MySQLdb
-
-# def listar_comida_db():
-#     conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae14", passwd="grupo09", database="zuplae14")
-#     cursor = conexao.cursor()
-#     cursor.execute("select * from COMIDA") 
-#     listar_comida = []
-#     for i in cursor.fetchall():
-#         produto = Produto()
-#         produto.id = i[0]
-#         produto.nome = i[1]
-#         produto.descricao = i[2]
-#         produto.categoria_id = i[3]
-#         produto.estoque_id = i[4]
-#         listar_produto.append(produto)
-
-#     conexao.close()
-#     return listar_produto
+from flask import Flask, render_template, request, redirect
+import MySQLdb
 
 
-pagina_nome = "Nome da pagina"
 
+pagina_nome = "HB FOOD"
 
-app = Flask(__name__)
+app= Flask(__name__)
 @app.route('/')
-
-
 def inicio():
-    return render_template('/index.html', pagina_nome = pagina_nome)
+    return render_template('index.html', pagina_nome = pagina_nome)
 
 @app.route('/login')
 def login():
-    return render_template('/login.html', pagina_nome = pagina_nome)
+    return render_template('login.html', pagina_nome = pagina_nome)
 
 @app.route('/login/cadastro')
-def cadastro():
-    return render_template('/cadastro.html', pagina_nome = pagina_nome)
+def pedido():
+    return render_template('pedido.html', pagina_nome = pagina_nome)
 
 
 @app.route('/comida')
 def comida():
-    return render_template('/comida.html', pagina_nome = pagina_nome)
+    return render_template('comida.html', pagina_nome = pagina_nome)
 
 @app.route('/bebida')
 def babida():
-    return render_template('/bebida.html', pagina_nome = pagina_nome)
+    return render_template('bebida.html', pagina_nome = pagina_nome)
 
 @app.route('/sobremesa')
 def sobremesa():
-    return render_template('/sobremesa.html', pagina_nome = pagina_nome)
+    return render_template('sobremesa.html', pagina_nome = pagina_nome)
 
 @app.route('/pedido')
 def pedido():
-    return render_template('/pedido.html', pagina_nome = pagina_nome)
+    return render_template('pedido.html', pagina_nome = pagina_nome)
 
-@app.route('/cadastro')
-def cadastrar():
-    return render_template('/cadastro.html', pagina_nome = pagina_nome)
 
 app.run()
