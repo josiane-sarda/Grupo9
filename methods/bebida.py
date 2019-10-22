cf48058bfdbba614a2d6c90e2ade600b47f2cc96
Lucas Alfredo da Silva <llucassilvaa12@gmail.com>
2019-10-21 22:08:17 -0300

    rotas e metodos completos

--- methods/bebida.py
+++ methods/bebida.py
@@ -1,18 +1,35 @@
 
 class Bebida: 
-    def __init__(self, id, nome, quantidade, preço):
-        self.id= id
-        self.nome = nome 
-        self.quantidade = quantidade
-        self.preço = preço
-   
+    def __init__(self):
+        self.__id= 0
+        self.__nome = '' 
+        self.__quantidade = 0
+        self.__preco = 0
 
+    @property
+    def id(self):
+        return self.__id
+    @id.setter
+    def id(self, id):
+        self.__id = id 
+        
+    @property
+    def nome(self):
+        return self.__nome
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome               
 
+    @property
+    def quantidade(self):
+        return self.__quantidade
+    @quantidade.setter
+    def quantidade(self, quantidade):
+        self.__quantidade = quantidade  
 
-
-    
-
-    
-    
-
-    
+    @property
+    def preco(self):
+        return self.__preco
+    @preco.setter
+    def preco(self, preco):
+        self.__preco = preco   
--- pagina.py
+++ pagina.py
@@ -1,73 +1,121 @@
 from flask import Flask, render_template, request, redirect
 import MySQLdb
-from methods.cadastro import Cadastro
 from methods.prato import Prato
 from methods.bebida import Bebida
 from methods.sobremesa import Sobremesa
 
 #################### PRATO ##########################################
 
+def salvar_prato_db(nome, preco, quantidade):
+    conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae14", passwd="grupo09", database="zuplae14")
+    cursor = conexao.cursor()
+    cursor.execute("INSERT INTO `PRATOS` (`NOME`, `QUANTIDADE`, `PRECO`)" + 
+    " VALUES ('{}', '{}', '{}')".format(nome, preco, quantidade))
+    conexao.commit()
+    conexao.close()
 
 def listar_prato_db():
     conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae14", passwd="grupo09", database="zuplae14")
     cursor = conexao.cursor()
-    cursor.execute("select * from PRATOS") 
+    cursor.execute("select * from `PRATOS`") 
     listar_prato = []
     for i in cursor.fetchall():
-        prato = Prato(i[0],i[1],i[2],i[3])       
-        listar_prato.append(prato)
-
+        novo_prato = Prato()
+        novo_prato.id = i[0]
+        novo_prato.nome = i[1] 
+        novo_prato.quantidade = i[2]
+        novo_prato.preco = i[3]      
+        listar_prato.append(novo_prato)
     conexao.close()
     return listar_prato
 
-def editar_prato_db(prato):
-    conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae04", passwd="lendas19", database="zuplae04")
+def editar_prato_db(id, nome, quantidade, preco):
+    conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae14", passwd="grupo09", database="zuplae14")
     cursor = conexao.cursor()
-    cursor.execute("UPDATE PRATO SET NOME = '{}', QUANTIDADE = '{}', PRECO = '{}' WHERE ID = {}"
-    .format(prato.nome, prato.quantidade, prato.preco ,prato.id))
+    cursor.execute("UPDATE `PRATOS` SET `NOME` = '{}', `QUANTIDADE` = {}, `PRECO` = {} WHERE `ID` = {}"
+    .format(nome, quantidade, preco ,id))
     conexao.commit()
     conexao.close()   
 
 def deletar_prato(id):
-    conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae04", passwd="lendas19", database="zuplae04")
+    conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae14", passwd="grupo09", database="zuplae14")
     cursor = conexao.cursor()
-    cursor.execute("DELETE FROM Prato WHERE id={}".format(id))
+    cursor.execute("DELETE FROM `PRATOS` WHERE `ID`={}".format(id))
     conexao.commit()
     conexao.close()
+
+
+def buscar_em_prato(id):
+    conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae14", passwd="grupo09", database="zuplae14")
+    cursor = conexao.cursor()
+    cursor.execute("select * from `PRATOS` WHERE `ID` ={}".format(id))
+    p = Prato()
+    for i in cursor.fetchall():
+        p.id = i[0]
+        p.nome = i[1] 
+        p.quantidade = i[2]
+        p.preco = i[3]      
+    conexao.commit()
+    conexao.close()
+    return p    
+
 
 
 #################### BEBIDA ##########################################
 
 
+def salvar_bebida_db(nome, preco, quantidade):
+    conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae14", passwd="grupo09", database="zuplae14")
+    cursor = conexao.cursor()
+    cursor.execute("INSERT INTO `BEBIDAS` (`NOME`, `QUANTIDADE`, `PRECO`)" + 
+    " VALUES ('{}', '{}', '{}')".format(nome, preco, quantidade))
+    conexao.commit()
+    conexao.close()
 
 def listar_bebida_db():
     conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae14", passwd="grupo09", database="zuplae14")
     cursor = conexao.cursor()
-
-    cursor.execute("select * from BEBIDAS") 
+    cursor.execute("select * from `BEBIDAS`") 
     listar_bebida = []
     for i in cursor.fetchall():
-        bebida = Bebida(i[0],i[1],i[2],i[3])
-        listar_bebida.append(bebida)
-
+        nova_bebida = Bebida()
+        nova_bebida.id = i[0]
+        nova_bebida.nome = i[1] 
+        nova_bebida.quantidade = i[2]
+        nova_bebida.preco = i[3]      
+        listar_bebida.append(nova_bebida)
     conexao.close()
     return listar_bebida
 
-def editar_bebida_db(bebida):
-    conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae04", passwd="lendas19", database="zuplae04")
+def editar_bebida_db(id, nome, quantidade, preco):
+    conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae14", passwd="grupo09", database="zuplae14")
     cursor = conexao.cursor()
-    cursor.execute("UPDATE BEBIDAS SET NOME = '{}', QUANTIDADE = '{}', PRECO = '{}' WHERE ID = {}"
-    .format(bebida.nome, bebida.quantidade,bebida.preco ,bebida.id))
+    cursor.execute("UPDATE `BEBIDAS` SET `NOME` = '{}', `QUANTIDADE` = {}, `PRECO` = {} WHERE `ID` = {}"
+    .format(nome, quantidade, preco ,id))
     conexao.commit()
     conexao.close()   
 
 def deletar_bebida(id):
-    conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae04", passwd="lendas19", database="zuplae04")
+    conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae14", passwd="grupo09", database="zuplae14")
     cursor = conexao.cursor()
-    cursor.execute("DELETE FROM BEBIDAS WHERE id={}".format(id))
+    cursor.execute("DELETE FROM `BEBIDAS` WHERE `ID`={}".format(id))
     conexao.commit()
     conexao.close()
 
+
+def buscar_em_bebida(id):
+    conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae14", passwd="grupo09", database="zuplae14")
+    cursor = conexao.cursor()
+    cursor.execute("select * from `BEBIDAS` WHERE `ID` ={}".format(id))
+    b = Bebida()
+    for i in cursor.fetchall():
+        b.id = i[0]
+        b.nome = i[1] 
+        b.quantidade = i[2]
+        b.preco = i[3]      
+    conexao.commit()
+    conexao.close()
+    return b    
 
 #################### SOBREMESA ##########################################
 
@@ -83,7 +131,7 @@ def salvar_sobremesa_db(nome, preco, quantidade):
 def listar_sobremesa_db():
     conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae14", passwd="grupo09", database="zuplae14")
     cursor = conexao.cursor()
-    cursor.execute("select * from SOBREMESAS") 
+    cursor.execute("select * from `SOBREMESAS`") 
     listar_sobremesa = []
     for i in cursor.fetchall():
         nova_sobremesa = Sobremesa()
@@ -107,7 +155,7 @@ def editar_sobremesa_db(id, nome, quantidade, preco):
 def deletar_sobremesa(id):
     conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae14", passwd="grupo09", database="zuplae14")
     cursor = conexao.cursor()
-    cursor.execute("DELETE FROM `SOBREMESAS` WHERE id={}".format(id))
+    cursor.execute("DELETE FROM `SOBREMESAS` WHERE `ID`={}".format(id))
     conexao.commit()
     conexao.close()
 
@@ -125,12 +173,6 @@ def buscar_em_sobremesa(id):
     conexao.close()
     return s                
 
-############################### PEDIDO ##########################################
-
-
-
-
-
 
 ################################-ROTAS-############################################################
 
@@ -146,15 +188,83 @@ def principal():
     
     return render_template('home.html', pagina_nome = pagina_nome)
 
-@app.route('/comida')
+@app.route('/prato')
 def comida():
     guto = listar_prato_db()
-    return render_template('comida.html', pagina_nome = pagina_nome, lista_html = guto)
+    return render_template('prato.html', pagina_nome = pagina_nome, lista_html = guto)
+
+@app.route('/prato/salvar', methods = ['POST'])
+def salvar_prato():
+    nome = request.form['item'] 
+    preco = request.form['preco']
+    quantidade = request.form['quant']
+    novo_prato = Sobremesa()
+    novo_prato.nome = nome
+    novo_prato.preco = preco
+    novo_prato.quantidade = quantidade
+    salvar_prato_db(novo_prato.nome, novo_prato.preco, novo_prato.quantidade)    
+    return redirect('/prato')
+
+@app.route('/deletar/prato')
+def apagar_prato():
+    id = request.args['id']
+    deletar_prato(id)
+    return redirect('/prato')
+
+@app.route('/alterar/prato')
+def alterar_prato():
+    id = request.args['id']
+    alteracao_prato = buscar_em_prato(id)
+    return render_template('alterar_prato.html', alterar_prato = alteracao_prato)    
+
+@app.route('/alterar/salvar/prato', methods=['POST'])
+def salvar_alteracao_prato():
+    id = request.form['id']
+    nome = request.form['nome']     
+    quantidade = request.form['quantidade'] 
+    preco = request.form['preco']
+    editar_prato_db(id, nome, quantidade, preco)
+    return redirect('/prato') 
+
 
 @app.route('/bebida')
 def babida():
     lenda = listar_bebida_db()
-    return render_template('bebida.html', pagina_nome = pagina_nome, lista_html = lenda)
+    return render_template('bebida.html', pagina_nome = pagina_nome, listas_html = lenda)
+
+@app.route('/bebida/salvar', methods = ['POST'])
+def salvar_bebida():
+    nome = request.form['item'] 
+    preco = request.form['preco']
+    quantidade = request.form['quant']
+    nova_bebida = Bebida()
+    nova_bebida.nome = nome
+    nova_bebida.preco = preco
+    nova_bebida.quantidade = quantidade
+    salvar_bebida_db(nova_bebida.nome, nova_bebida.preco, nova_bebida.quantidade)    
+    return redirect('/bebida')
+
+@app.route('/deletar/bebida/')
+def apagar_bebida():
+    id = request.args['id']
+    deletar_bebida(id)
+    return redirect('/bebida')
+
+@app.route('/alterar/bebida/')
+def alterar_bebida():
+    id = request.args['id']
+    alteracao_bebida = buscar_em_bebida(id)
+    return render_template('alterar_bebida.html', alterar_bebida = alteracao_bebida)    
+
+@app.route('/alterar/salvar/bebida', methods=['POST'])
+def salvar_alteracao_bebida():
+    id = request.form['id']
+    nome = request.form['nome']     
+    quantidade = request.form['quantidade'] 
+    preco = request.form['preco']
+    editar_bebida_db(id, nome, quantidade, preco)
+    return redirect('/bebida') 
+
 
 @app.route('/sobremesa')
 def sobremesa():
@@ -194,17 +304,5 @@ def salvar_alteracao_sobremesa():
     editar_sobremesa_db(id, nome, quantidade, preco)
     return redirect('/sobremesa') 
 
-@app.route('/pedido')
-def pedido():
-    return render_template('pedido.html', pagina_nome = pagina_nome)
 
-@app.route('/pedido/confirmar', methods=['POST'])
-def confirmar_pedido():
-    return 'Pedido confirmado! - Seu pedido está a caminho!'
-
-@app.route('/pedido/cancelar', methods=['POST'])
-def cancelar_pedido():
-    return 'Pedido cancelado!'
-
-
-app.run()
+app.run(debug=True)
